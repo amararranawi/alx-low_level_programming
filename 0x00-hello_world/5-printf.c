5e02c42807b8a1290c464b421acbbf2e1e8ee302
Abubakar Muhammad Wada <amararranawy@gmail.com>
2022-12-08 14:09:21 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  * main - main block
- * return : always zero (success)
+ * Description : this will print the value in the paranthesis
+ * main : main function to be executed
+ * Return: returns 0
  */
-int main(void)
+int main(void) /* main void retuen value  function*/
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
