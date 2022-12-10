23ea407d2ebc7353112a560192a3e1b527320e20
Abubakar Muhammad Wada <amararranawy@gmail.com>
2022-12-10 03:04:46 -0800

    remove spaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <time.h> 
-/* more headers goes there */  
-/* betty style doc for function main goes there */ 
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
 int main(void)
 {
 int n;
-srand(time(0)); 
-n = rand() - RAND_MAX / 2; 
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if(n > 0) {
 printf("%i is positive\n", n);
-} else if(n < 0) {
+}
+else if(n < 0) {
 printf("%i is negative\n", n);
-} else {
+}
+else {
 printf("%i is zero\n", n);
 }
-return (0);  
+return (0);
 }
