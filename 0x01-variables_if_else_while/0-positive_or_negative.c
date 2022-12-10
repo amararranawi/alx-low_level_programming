c8751bf1663fd77f4c1bca9fc52f9c80a3d60445
Abubakar Muhammad Wada <amararranawy@gmail.com>
2022-12-10 02:39:03 -0800

    add stdio header

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
+#invlude <stdio.h>
 #include <stdlib.h> 
- #include <time.h> 
+#include <time.h> 
 /* more headers goes there */ 
   
 /* betty style doc for function main goes there */ 
-int main(void) 
+int main(void)
 { 
  int n; 
   
@@ -11,13 +12,13 @@ int main(void)
  n = rand() - RAND_MAX / 2; 
  /* your code goes there */
 if(n > 0) {
-prinf("%d is positive\n", n);
+prinf("%i is positive\n", n);
 }
-else if(n == 0) {
-printf("%d is zero\n", n);
+else if(n < 0) {
+printf("%i is negative\n", n);
 }
-else(n < 0) {
-     printf("%d is negative\n", n);
+else {
+printf("%i is zero\n", n);
 }
-return (0); 
+return (0);  
 }
