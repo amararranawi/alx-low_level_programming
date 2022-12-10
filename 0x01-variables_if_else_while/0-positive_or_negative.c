200993fde8961e6bf1e0d732e69d259cbf84f2d5
Abubakar Muhammad Wada <amararranawy@gmail.com>
2022-12-10 03:31:59 -0800

    add description

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
-int main(void)
+/**
+*main - main block
+* Description : this will print the value in the paranthesis
+* main : main function to be executed
+* Return: returns 0
+*/
+int main(void) /*  main void return value function */
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if(n > 0) {
+if (n > 0)
+{
 printf("%i is positive\n", n);
 }
-else if(n < 0) {
+else if (n < 0)
+{
 printf("%i is negative\n", n);
 }
-else {
+else
+{
 printf("%i is zero\n", n);
 }
 return (0);
