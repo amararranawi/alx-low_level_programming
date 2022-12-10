99812a67e819c98c8c158d242707f5d2eaebb400
Abubakar Muhammad Wada <amararranawy@gmail.com>
2022-12-10 05:54:39 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main -main block will execute for
+* main: main function
+* return: terminate main function
+* description: print alphabet in lower case
+*/
+int main(void) /* main void return executable codes*/
+{
+char alp;
+for (alp = a; alp <= z; ++alp) /* for will print range*/
+{
+putchar(alp); /* print statement*/
+putchar("\n");
+}
+return (0);
+}
